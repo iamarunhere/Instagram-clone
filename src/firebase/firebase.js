473187883f5b286093bd84b02ac66c02ddd42295
your_username <your_email@example.com>
2024-09-25 12:10:15 +0530

    insta-clone

---
+++ src/firebase/firebase.js
@@ -0,0 +1,39 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, firestore, storage };
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDTcpbh0dJelrMkXlxA9tLRBVkRYhrDpTA",
+//   authDomain: "instagram-clone-f6c9d.firebaseapp.com",
+//   projectId: "instagram-clone-f6c9d",
+//   storageBucket: "instagram-clone-f6c9d.appspot.com",
+//   messagingSenderId: "252247865032",
+//   appId: "1:252247865032:web:43e606c04b047b9465d7fd"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
